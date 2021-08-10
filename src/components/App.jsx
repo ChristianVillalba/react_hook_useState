980faddc185e726d4ab485ede5139ed3e5ae65b0
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-08-10 13:25:11 +0100

    initial state updated, function decrease and increase updated and working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
+// React Basic Hooks Documentation:
+// https://reactjs.org/docs/hooks-reference.html
+
 import React, { useState } from "react";
 
 function App() {
-  const [count] = useState(987);
+  const [count, setCount] = useState(0);
+  function decrease() {
+    setCount(count - 1);
+  }
   function increase() {
-    // count++;
+    setCount(count + 1);
   }
   return (
     <div className="container">
       <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
       <button onClick={increase}>+</button>
     </div>
   );
@@ -18,3 +25,6 @@ export default App;
 // ES6 - Destructuring
 // const [red, green, blue] = [9 , 132, 227]
 // console.log(green)
+
+// uesState returns a Value and a Function so :
+// const [count, setCount] = useState(0);
